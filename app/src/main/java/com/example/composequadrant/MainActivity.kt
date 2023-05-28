e07371faf2a5985fa8b34f4d05079aff8b7cfdd5
Augustinekan <augustinekanyuira0@gmail.com>
2023-05-28 13:01:57 +0300

    display and align text

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -69,12 +74,30 @@ Column() {
 fun QuadrantGridChild(title : String, subtitle: String,color: Color,modifier: Modifier = Modifier){
 
     val configuration = LocalConfiguration.current
-
     val screenHeight = configuration.screenHeightDp.dp
     val screenWidth = configuration.screenWidthDp.dp
     Box(
-        modifier = modifier.background(color = color).height(height = (screenHeight/2)).width(width = (screenWidth/2))
+        modifier = modifier
+            .background(color = color)
+            .height(height = (screenHeight / 2))
+            .width(width = (screenWidth / 2))
     ){
-
+     Column(
+         modifier = modifier.padding(16.dp).align(alignment = Alignment.Center),
+         horizontalAlignment = Alignment.CenterHorizontally
+     ) {
+         TileTitle(title = title)
+         TileSubText(subtitle = subtitle)
+     }
     }
+}
+
+@Composable
+fun TileTitle(title:String,modifier: Modifier = Modifier){
+    Text(text = title , fontWeight = FontWeight.Bold,modifier = modifier.padding(bottom = 16.dp))
+}
+
+@Composable
+fun TileSubText(subtitle: String){
+    Text(text = subtitle, textAlign = TextAlign.Justify,  fontSize = 16.sp)
 }
